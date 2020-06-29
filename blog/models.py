2382068cdcd66d50c02db4f399d72c586e3711f8
Ashwin Rathore <34775355+111ashwin@users.noreply.github.com>
2020-06-29 22:16:54 +0530

    Update models.py
    check git pull push

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
 	text = models.TextField()
 	create_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
-
+# 	hey ashwin
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
